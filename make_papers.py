b0266831c33b29fa3ad93db84bd22c346c59d95c
John Horton <john.joseph.horton@gmail.com>
2013-03-20 08:55:42 -0700

    cleanup

--- make_papers.py
+++ make_papers.py
@@ -21,7 +21,21 @@ refresh_index("index.md")
 
 papers = open("papers.md", "w")
 cv = open("cv.md", "r")
-papers.write("""<link href="markdown.css" rel="stylesheet"></link>\n\n""")
+papers.write("""<link href="markdown.css" rel="stylesheet"></link>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-9193153-6']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+\n\n""")
 papers.writelines(["Research Papers\n", "===============\n\n"])
 
 write_line = False 
